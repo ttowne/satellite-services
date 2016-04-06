02e1808be1b5837685c463beddcbd35806c8f586
Travis Towne <travis.towne@apptechsys.com>
2016-04-06 09:08:53 -0400

    Added eslint and a ruleset created by 2.6 and updated the 2.7 changes.
    Still need to go throw the rule set for 2.7 with Jason and make sure these are what we want.

--- conf/gulp/index.js
+++ conf/gulp/index.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp'),
-    serve = require('gulp-serve');
+    serve = require('gulp-serve'),
+    eslint = require('gulp-eslint');
  
 gulp.task('serve', serve([
   'src',
   'examples'
 ]));
+
+gulp.task('eslint', function () {
+    return gulp.src([
+        'src/**/*.js',
+        'examples/**/*.js'
+    ]).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError());
+});
