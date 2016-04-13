96b19bb5ff34c2a6fbb781859bd41a7f5621d4a8
Jason Hinebaugh <jason.hinebaugh@gmail.com>
2016-04-13 07:32:38 -0400

    Failed attempt at simple AJAX to wikipedia API

--- src/module/satellite.js
+++ src/module/satellite.js
@@ -2,7 +2,7 @@
     var outputNode = document.getElementById('fetch-result'),
         params,
         request = new XMLHttpRequest(),
-        query = '?',
+        query,
         url = 'https://en.wikipedia.org/w/api.php',
         worker = new SharedWorker('../worker/main.js');
 
@@ -15,13 +15,19 @@
         rvprop: 'content',
         rvlimit: 1,
         format: 'json',
-        origin: 'localhost'
+        origin: 'http://0.0.0.0:3000'
     };
 
+    query = Object.keys(params).reduce(function (sum, current, i) {
+        return `${sum}${i ? '&' : ''}${current}=${encodeURIComponent(params[current])}`;
+    }, '?');
+
     request.onreadystatechange = function () {
+        console.log(request);
         if (request.readyState === 4) {
             outputNode.innerText = request.responseText;
         }
     }
-    request.send('GET', 'https://en.wikipedia.org/w/api.php' + query);
+    request.open('GET', 'https://en.wikipedia.org/w/api.php' + query);
+    request.send();
 }());
